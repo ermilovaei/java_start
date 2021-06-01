5c4fb9469a4bc365bd8e431656b2653aa27fa907
ermilovaei <eei@bk.ru>
2021-06-01 19:45:20 +0400

    написаны тесты для разных координат

--- sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
+++ sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
@@ -2,9 +2,9 @@ package ru.stqa.stjv.sandbox;
 
 public class MyMain {
   public static void main(String[] args) {
-    Point  myP1 = new Point(1,1);
+    Point  myP1 = new Point(0,0);
 
-    Point  myP2 = new Point(-33, 0);
+    Point  myP2 = new Point(-33, 12);
 
     System.out.println(" P1P2 distance = " + myP1.distance(myP2));
   }
--- sandbox/src/test/java/ru/stqa/stjv/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/stjv/sandbox/PointTests.java
@@ -17,4 +17,27 @@ public class PointTests {
     Point p2 = new Point(-33,12);
     Assert.assertEquals(p.distance(p2), 35.11409973, 0.00001);
   };
+  @Test
+  public void testDistance3() {
+    Point p = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p.distance(p2), 0);
+  }
+  @Test
+  public void testDistance4(){
+    Point p = new Point (4560,4560);
+    Point p2 = new Point(4560,4560);
+    Assert.assertEquals(p.distance(p2), 0);
+
+  };
+
+  @Test
+  public void testDistance5(){
+    Point p = new Point (-0.15,123);
+    Point p2 = new Point(0.45,-50 );
+    Assert.assertEquals(p.distance(p2), 173.00104,0.00001);
+
+  };
+
 }
+
