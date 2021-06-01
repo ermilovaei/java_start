5eab7617c0486de4a8cb9e45b095d6ebd4f05869
ermilovaei <eei@bk.ru>
2021-06-01 15:52:02 +0400

    Создан метод distance и его вызов

--- sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
+++ sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
@@ -6,11 +6,9 @@ public class MyMain {
 
     Point  myP2 = new Point(1, 11);
 
-    System.out.println(" XY distance = " + distance(myP1,myP2));
+    System.out.println(" P1P2 distance = " + myP1.distance(myP2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return  (Math.sqrt (Math.pow ((p2.coordX - p1.coordX), 2) + Math.pow ((p2.coordY - p1.coordY),2)));
-  }
+
 
 }
