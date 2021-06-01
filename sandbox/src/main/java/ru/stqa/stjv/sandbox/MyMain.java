08c031abed4915c56daf65be6e638e174e58d99f
ermilovaei <eei@bk.ru>
2021-06-01 15:32:13 +0400

    добавлен конструктор класса

--- sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
+++ sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
@@ -2,19 +2,15 @@ package ru.stqa.stjv.sandbox;
 
 public class MyMain {
   public static void main(String[] args) {
-    Point  myP1 = new Point();
-    myP1.coordX = 1;
-    myP1.coordY = 1;
+    Point  myP1 = new Point(1,1);
 
-    Point  myP2 = new Point();
-    myP2.coordX = 1;
-    myP2.coordY = 11;
+    Point  myP2 = new Point(1, 11);
 
     System.out.println(" XY distance = " + distance(myP1,myP2));
   }
 
   public static double distance(Point p1, Point p2) {
-    return  (Math.sqrt (Math.pow ((p2.coordX - p1.coordX), 2) +Math.pow ((p2.coordY - p1.coordY),2)));
+    return  (Math.sqrt (Math.pow ((p2.coordX - p1.coordX), 2) + Math.pow ((p2.coordY - p1.coordY),2)));
   }
 
 }
--- sandbox/src/main/java/ru/stqa/stjv/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/stjv/sandbox/Point.java
@@ -3,6 +3,11 @@ package ru.stqa.stjv.sandbox;
 public class Point {
   public double coordX;
   public double coordY;
+  public Point (double x, double y){
+    this.coordY = x;
+    this.coordY = y;
+  }
+
 }
 
 
