cfa21ddcf7e11f91780c41a6f274b0a5243499ae
ermilovaei <eei@bk.ru>
2021-06-02 18:33:26 +0400

    эксперимент для проверки игнора папки build

--- sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
+++ sandbox/src/main/java/ru/stqa/stjv/sandbox/MyMain.java
@@ -2,7 +2,7 @@ package ru.stqa.stjv.sandbox;
 
 public class MyMain {
   public static void main(String[] args) {
-    Point  myP1 = new Point(0,0);
+    Point  myP1 = new Point(1,1);
 
     Point  myP2 = new Point(-33, 12);
 
