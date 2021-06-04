e7375f32a60525d2fef3cd76da4849ebe5bbdd96
ermilovaei <eei@bk.ru>
2021-06-04 19:07:26 +0400

    выделен объект group data

--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/GroupCreationTests.java
@@ -31,7 +31,7 @@ public class GroupCreationTests {
   public void testGroupCreation() throws Exception {
     goToGroupsPage();
     initGroupCreation();
-    fillGroupData("test group name", "test group header", "test group footer");
+    fillGroupData(new groupData("test group name", "test group header", "test group footer"));
     submitGroupCreation();
     returnToGroupPage();
 
@@ -47,16 +47,16 @@ public class GroupCreationTests {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillGroupData(String name, String header, String footer) {
+  private void fillGroupData(groupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(name);
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(header);
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(footer);
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
   private void initGroupCreation() {
