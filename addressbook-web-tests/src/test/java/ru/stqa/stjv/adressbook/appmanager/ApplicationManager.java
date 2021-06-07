4e5c685d3531da8862cc6e1b8e47fffd5cc93328
ermilovaei <eei@bk.ru>
2021-06-07 16:48:33 +0400

    Создан класс GroupHelper

--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/appmanager/ApplicationManager.java
@@ -3,110 +3,85 @@ package ru.stqa.stjv.adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import ru.stqa.stjv.adressbook.model.contactData;
-import ru.stqa.stjv.adressbook.model.groupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  private WebDriver wd;
+
+  private final GroupHelper groupHelper = new GroupHelper();
 
   public void init() {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    groupHelper.wd = new FirefoxDriver();
+    groupHelper.wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     login();
   }
 
   public void returnToHomePage() {
 
-    wd.findElement(By.linkText("home page")).click();
+    groupHelper.wd.findElement(By.linkText("home page")).click();
   }
 
   public void submitContactCreation() {
 
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    groupHelper.wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
   public void fillContactData(contactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAdress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getTelephoneHome());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmailFirst());
-    wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getbDate());
-    wd.findElement(By.name("bday")).click();
-    wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getbMonth());
-    wd.findElement(By.name("bmonth")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactData.getbYear());
+    groupHelper.wd.findElement(By.name("firstname")).click();
+    groupHelper.wd.findElement(By.name("firstname")).clear();
+    groupHelper.wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    groupHelper.wd.findElement(By.name("lastname")).click();
+    groupHelper.wd.findElement(By.name("lastname")).clear();
+    groupHelper.wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+    groupHelper.wd.findElement(By.name("address")).click();
+    groupHelper.wd.findElement(By.name("address")).clear();
+    groupHelper.wd.findElement(By.name("address")).sendKeys(contactData.getAdress());
+    groupHelper.wd.findElement(By.name("home")).click();
+    groupHelper.wd.findElement(By.name("home")).clear();
+    groupHelper.wd.findElement(By.name("home")).sendKeys(contactData.getTelephoneHome());
+    groupHelper.wd.findElement(By.name("email")).click();
+    groupHelper.wd.findElement(By.name("email")).clear();
+    groupHelper.wd.findElement(By.name("email")).sendKeys(contactData.getEmailFirst());
+    groupHelper.wd.findElement(By.name("bday")).click();
+    new Select(groupHelper.wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getbDate());
+    groupHelper.wd.findElement(By.name("bday")).click();
+    groupHelper.wd.findElement(By.name("bmonth")).click();
+    new Select(groupHelper.wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getbMonth());
+    groupHelper.wd.findElement(By.name("bmonth")).click();
+    groupHelper.wd.findElement(By.name("byear")).click();
+    groupHelper.wd.findElement(By.name("byear")).clear();
+    groupHelper.wd.findElement(By.name("byear")).sendKeys(contactData.getbYear());
   }
 
   public void initContactCreation() {
 
-    wd.findElement(By.linkText("add new")).click();
+    groupHelper.wd.findElement(By.linkText("add new")).click();
   }
 
   private void login() {
-    wd.get("http://localhost/addressbook/addressbook/index.php");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    groupHelper.wd.get("http://localhost/addressbook/addressbook/index.php");
+    groupHelper.wd.findElement(By.name("user")).clear();
+    groupHelper.wd.findElement(By.name("user")).sendKeys("admin");
+    groupHelper.wd.findElement(By.name("pass")).click();
+    groupHelper.wd.findElement(By.name("pass")).clear();
+    groupHelper.wd.findElement(By.name("pass")).sendKeys("secret");
+    groupHelper.wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   public void stop() {
-    wd.quit();
-  }
-
-  public void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupData(groupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
+    groupHelper.wd.quit();
   }
 
   public void goToGroupsPage() {
-    wd.findElement(By.linkText("groups")).click();
+    groupHelper.wd.findElement(By.linkText("groups")).click();
   }
 
   private boolean isElementPresent(By by) {
     try {
-      wd.findElement(by);
+      groupHelper.wd.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -115,10 +90,14 @@ public class ApplicationManager {
 
   private boolean isAlertPresent() {
     try {
-      wd.switchTo().alert();
+      groupHelper.wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
     }
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/tests/GroupCreationTests.java
@@ -8,10 +8,10 @@ public class GroupCreationTests extends TestBase{
   @Test
   public void testGroupCreation() throws Exception {
     app.goToGroupsPage();
-    app.initGroupCreation();
-    app.fillGroupData(new groupData("test group name", "test group header", "test group footer"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupData(new groupData("test group name", "test group header", "test group footer"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
 
   }
 
