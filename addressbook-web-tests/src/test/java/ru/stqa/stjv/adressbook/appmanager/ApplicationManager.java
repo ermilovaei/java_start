99441b5d9a20ac416317eee385f76312bb25b7f2
ermilovaei <eei@bk.ru>
2021-06-16 09:56:12 +0400

    добавлена возможность запуска в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/appmanager/ApplicationManager.java
@@ -1,27 +1,43 @@
 package ru.stqa.stjv.adressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private final String browser;
   WebDriver wd;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
 
-  public void init() {
-   wd = new FirefoxDriver();
-   wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-   groupHelper = new GroupHelper(wd);
-   contactHelper = new ContactHelper(wd);
-   navigationHelper = new NavigationHelper(wd);
-   sessionHelper = new SessionHelper(wd);
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
-   wd.get("http://localhost/addressbook/addressbook/index.php");
-   sessionHelper.login("admin", "secret");
+  public void init() {
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+    ;
+
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    groupHelper = new GroupHelper(wd);
+    contactHelper = new ContactHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+
+    wd.get("http://localhost/addressbook/addressbook/index.php");
+    sessionHelper.login("admin", "secret");
   }
 
   public void stop() {
@@ -39,7 +55,6 @@ public class ApplicationManager {
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
-
 
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.stjv.adressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.stjv.adressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
