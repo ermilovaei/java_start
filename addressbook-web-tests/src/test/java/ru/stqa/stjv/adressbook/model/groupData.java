7ee2ea67e9d5154cbca80673eeb34f9a213b330f
ermilovaei <eei@bk.ru>
2021-07-01 10:12:14 +0400

    доработан groupcreationtests, добавлено сравнение списков после создания

--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/model/groupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/model/groupData.java
@@ -3,28 +3,32 @@ package ru.stqa.stjv.adressbook.model;
 import java.util.Objects;
 
 public class groupData {
-  private final String id;
+  private int id;
   private final String name;
-
-  @Override
-  public String toString() {
-    return "groupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            ", header='" + header + '\'' +
-            ", footer='" + footer + '\'' +
-            '}';
-  }
-
   private final String header;
   private final String footer;
+
+  public int getId() {
+    return id;
+  }
+
+  public groupData(int id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     groupData groupData = (groupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+    return id == groupData.id && Objects.equals(name, groupData.name);
   }
 
   @Override
@@ -32,19 +36,8 @@ public class groupData {
     return Objects.hash(id, name);
   }
 
-  public String getId() {
-    return id;
-  }
-
-  public groupData(String id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
-  public groupData( String name, String header, String footer) {
-    this.id = null;
+  public groupData(String name, String header, String footer) {
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -61,4 +54,15 @@ public class groupData {
   public String getFooter() {
     return footer;
   }
+
+  @Override
+  public String toString() {
+    return "groupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            ", header='" + header + '\'' +
+            ", footer='" + footer + '\'' +
+            '}';
+  }
+
 }
--- addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/stjv/adressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.stjv.adressbook.model.groupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -12,9 +13,20 @@ public class GroupCreationTests extends TestBase{
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupsPage();
     List<groupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new groupData("test group name", "test group header", "test group footer"));
+    groupData group = new groupData("test group name", "test group header", "test group footer");
+    app.getGroupHelper().createGroup(group);
     List<groupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size() + 1);
+    int max = 0;
+
+    for (groupData g: after){
+      if (g.getId()>max){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(after),new HashSet<>(before));
   }
 
 }
