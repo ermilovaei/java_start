e18628f5b3d1d2e255263f0907d7969a9cae2d49
ermilovaei <eei@bk.ru>
2021-08-05 15:54:38 +0400

    разработка коннекта к bugify

--- rest-sample/src/test/java/ru/stqa/stjv/rest/Issure.java
+++
@@ -1,4 +0,0 @@
-package ru.stqa.stjv.rest;
-
-public class Issure {
-}
--- rest-sample/src/test/java/ru/stqa/stjv/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/stjv/rest/RestTests.java
@@ -1,23 +1,36 @@
 package ru.stqa.stjv.rest;
 
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.Set;
 
 public class RestTests
 {
   @Test
-  public void createIssue(){
-    Set<Issure> oldIssues = getIssures();
-    Issure newIssue = new Issure();
-    int issureID = createIssure(newIssue);
-    Set<Issure> newIssues = getIssures();
-    oldIssues.add(newIssue.withId(issureID));
+  public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue();
+    int issueID = createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueID));
     Assert.assertEquals(newIssues, oldIssues);
 }
 
-  private Set<Issure> getIssures() {
+  private int createIssue(Issue newIssue) {
+    return 0;
+  }
+
+  private Set<Issue> getIssues() throws IOException {
+    String json = getExecutor()
+            .execute(Request.Get("http://demo.bugify.com/api/issues.json")).returnContent().asString();
     return null;
+  }
+
+  private Executor getExecutor() {
+   return Executor.newInstance().auth("28accbe43ea112d9feb328d2c00b3eed","");
   }
 }
